82affc65a4d648c11c6954e7ae402779bf3630c6
YutoIzumi <mizun524@gmail.com>
2018-08-19 21:00:40 +0900

    distinguish between incoming wave and mapping wave

--- forSHB/simulate_SHB.py
+++ forSHB/simulate_SHB.py
@@ -7,16 +7,19 @@ from scipy.special import sph_harm, spherical_jn, spherical_yn
 
 import matplotlib.pyplot as plt
 
+import sys
+sys.path.append('/Users/mizun524/research/pfiles')
 from mymodules import cart2sph, sph_harm_real
 
 # 0. values
-order = 6
-freq = 1000
-inc_dir = np.array([0, np.pi/2])
+order = 2
+order_in = 40
+freq = 8000
+inc_dir = np.array([0, np.pi / 2])
 
 # 1. import data
 # import the micophones arrangement
-mPos_mat = io.loadmat("../ViRealMic/ViRealMic.mat")
+mPos_mat = io.loadmat("../../ViRealMic/ViRealMic.mat")
 mPos_cart = np.array(mPos_mat["Pmic"])
 # cartesian to spherical coordinate
 mPos_sph = np.zeros_like(mPos_cart)
@@ -36,24 +39,30 @@ def calc_j(n, k, a):
                                        spherical_jn(n, k * a, derivative=True) / shankel_d * shankel)
     return j
 
+
 # mode strength b(ka)
 c = 347  # sound speed
 k = 2 * np.pi * freq / c
 b = np.array([calc_j(int(np.floor(np.sqrt(nn))), k, mPos_sph[0, 2])
-              for nn in range((order + 1)**2)])
+              for nn in range((order_in + 1)**2)])
 
 # calculate SH at the positions of microphones
-Y_mic = np.array([sph_harm(m, n, mPos_sph[:, 0], mPos_sph[:, 1])
-                  for n in range(order + 1) for m in range(-n, n + 1)])
+Y_mic_in = np.array([sph_harm(m, n, mPos_sph[:, 0], mPos_sph[:, 1])
+                  for n in range(order_in + 1) for m in range(-n, n + 1)])
 
 # calculate SH for incoming wave
 Y_in = np.array([sph_harm(m, n, inc_dir[0], inc_dir[1])
-                  for n in range(order + 1) for m in range(-n, n + 1)])
+                 for n in range(order_in + 1) for m in range(-n, n + 1)])
 
 # calculate microphone's input each order and degree
-pnm_in = np.array([b * Y_mic[:, idirs] * np.conjugate(Y_in) for idirs in range(mPos_sph.shape[0])])
+pnm_in = np.array([b * Y_mic_in[:, idirs] * np.conjugate(Y_in)
+                   for idirs in range(mPos_sph.shape[0])])
 # calculate microphone's input
 p_in = np.sum(pnm_in, axis=1)
+
+# add noise
+p_noise = np.random.rand(64) * np.max(p_in) * 3
+#p_in += p_noise
 
 # 3. calculate SHB's beampattern
 # observation dirs
@@ -68,9 +77,20 @@ dirs_obs[:, 0] = np.matlib.repmat(azi, 1, len(ele))
 # dirs_obs[:, 1](theta) : 0, 0, ..., 0, pi/60, ..., 2pi, 2pi
 dirs_obs[:, 1] = np.array(
     [np.floor(n / len(azi)) * np.pi / 60 for n in range(len_dirs)])
+
 # calculate SH at the observation points
 Y_obs = np.array([sph_harm(m, n, dirs_obs[:, 0], dirs_obs[:, 1])
                   for n in range(order + 1) for m in range(-n, n + 1)])
+
+# calculate SH at the positions of microphones
+Y_mic = np.array([sph_harm(m, n, mPos_sph[:, 0], mPos_sph[:, 1])
+                  for n in range(order + 1) for m in range(-n, n + 1)])
+
+# mode strength b(ka)
+c = 347  # sound speed
+k = 2 * np.pi * freq / c
+b = np.array([calc_j(int(np.floor(np.sqrt(nn))), k, mPos_sph[0, 2])
+              for nn in range((order + 1)**2)])
 
 # calculate the beampattern
 pnm = np.dot(p_in, np.linalg.pinv(Y_mic))
