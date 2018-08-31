9f4cd0b90b8bc4299b77a236425286f64d963a88
YutoIzumi <mizun524@gmail.com>
2018-08-31 17:51:21 +0900

    add func

--- mymodules/__init__.py
+++ mymodules/__init__.py
@@ -1,5 +1,6 @@
 import sys; sys.path.append('/Users/mizun524/research/pfiles/mymodules')
 
 from cart2sph import cart2sph
+from sph2cart import sph2cart
 from sph_harm_real import sph_harm_real
 from illustrate_func import mapping_frequency
